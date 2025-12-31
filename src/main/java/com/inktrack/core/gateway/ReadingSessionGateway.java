8397d9b325516ff1370382db919681103fd082d1
Felipe Melo <felipemelozx@gmail.com>
2025-12-31 17:16:42 -0300

    feat: add 'getByIdAndByBookIdAndUserId' to 'ReadingSessionGateway'

--- src/main/java/com/inktrack/core/gateway/ReadingSessionGateway.java
+++ src/main/java/com/inktrack/core/gateway/ReadingSessionGateway.java
@@ -3,6 +3,7 @@ package com.inktrack.core.gateway;
 import com.inktrack.core.domain.ReadingSession;
 import com.inktrack.core.utils.PageResult;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ReadingSessionGateway {
@@ -10,4 +11,9 @@ public interface ReadingSessionGateway {
   ReadingSession save(ReadingSession readingSession);
 
   PageResult<ReadingSession> getReadingByBookIdAndUserId(Long bookId, UUID userId, int page, int size);
+
+  Optional<ReadingSession> getByIdAndByBookIdAndUserId(Long readingSessionId, Long bookId, UUID userId);
+
+  ReadingSession update(ReadingSession readingSession);
+
 }
