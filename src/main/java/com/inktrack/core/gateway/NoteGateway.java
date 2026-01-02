38afd8115992e9516b73dedcff5222106879dc48
Felipe Melo <felipemelozx@gmail.com>
2026-01-02 16:08:42 -0300

    feat: Add getNotesByBooidAndUserId method in NoteGateway

--- src/main/java/com/inktrack/core/gateway/NoteGateway.java
+++ src/main/java/com/inktrack/core/gateway/NoteGateway.java
@@ -1,9 +1,13 @@
 package com.inktrack.core.gateway;
 
 import com.inktrack.core.domain.Note;
+import com.inktrack.core.utils.PageResult;
+
+import java.util.UUID;
 
 public interface NoteGateway {
 
   Note save(Note note);
 
+  PageResult<Note> getNotesByBooidAndUserId(Long bookId, UUID userId, int page);
 }
--- src/main/java/com/inktrack/infrastructure/gateway/NoteGatewayImpl.java
+++ src/main/java/com/inktrack/infrastructure/gateway/NoteGatewayImpl.java
@@ -2,10 +2,17 @@ package com.inktrack.infrastructure.gateway;
 
 import com.inktrack.core.domain.Note;
 import com.inktrack.core.gateway.NoteGateway;
+import com.inktrack.core.utils.PageResult;
 import com.inktrack.infrastructure.entity.NoteEntity;
 import com.inktrack.infrastructure.mapper.NoteMapper;
 import com.inktrack.infrastructure.persistence.NoteRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 public class NoteGatewayImpl implements NoteGateway {
@@ -22,5 +29,20 @@ public class NoteGatewayImpl implements NoteGateway {
   public Note save(Note note) {
     NoteEntity noteEntity = noteMapper.domainToEntity(note);
     return noteMapper.entityToDomain(noteRepository.save(noteEntity));
+  }
+
+  @Override
+  public PageResult<Note> getNotesByBooidAndUserId(Long bookId, UUID userId, int page) {
+    Pageable pageRequest = PageRequest.of(page, 5, Sort.by(Sort.Direction.DESC, "createdAt"));
+    Page<NoteEntity> pageEntity = noteRepository.findByBookIdAndUserId(bookId, userId, pageRequest);
+
+    return new PageResult<>(
+        5,
+        pageEntity.getTotalPages(),
+        page,
+        pageEntity.getContent().stream()
+            .map(noteMapper::entityToDomain)
+            .toList()
+    );
   }
 }
