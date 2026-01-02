886d7a570af0743abbba5729ed0ee84b606a658a
Felipe Melo <felipemelozx@gmail.com>
2026-01-02 16:07:53 -0300

    feat: Add getNotes endpoint for NoteController

--- src/main/java/com/inktrack/infrastructure/controller/NoteController.java
+++ src/main/java/com/inktrack/infrastructure/controller/NoteController.java
@@ -1,19 +1,24 @@
 package com.inktrack.infrastructure.controller;
 
 import com.inktrack.core.usecases.note.CreateNoteUseCase;
+import com.inktrack.core.usecases.note.GetNotePaginatorUseCase;
 import com.inktrack.core.usecases.note.NoteInput;
 import com.inktrack.core.usecases.note.NoteOutput;
+import com.inktrack.core.utils.PageResult;
 import com.inktrack.infrastructure.dtos.notes.CreateNoteRequest;
 import com.inktrack.infrastructure.dtos.notes.NoteResponse;
 import com.inktrack.infrastructure.entity.UserEntity;
 import com.inktrack.infrastructure.mapper.NoteMapper;
 import com.inktrack.infrastructure.utils.response.ApiResponse;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,10 +26,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class NoteController {
 
   private final CreateNoteUseCase createNoteUseCase;
+  private final GetNotePaginatorUseCase getNotePaginatorUseCase;
   private final NoteMapper noteMapper;
 
-  public NoteController(CreateNoteUseCase createNoteUseCase, NoteMapper noteMapper) {
+  public NoteController(
+      CreateNoteUseCase createNoteUseCase,
+      GetNotePaginatorUseCase getNotePaginatorUseCase,
+      NoteMapper noteMapper
+  ) {
     this.createNoteUseCase = createNoteUseCase;
+    this.getNotePaginatorUseCase = getNotePaginatorUseCase;
     this.noteMapper = noteMapper;
   }
 
@@ -38,5 +49,22 @@ public class NoteController {
     NoteResponse noteResponse = noteMapper.outputToResponse(noteOutput);
     ApiResponse<NoteResponse> apiResponse = ApiResponse.success(noteResponse);
     return ResponseEntity.ok(apiResponse);
+    return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
+  }
+
+  @GetMapping
+  public ResponseEntity<ApiResponse<PageResult<NoteResponse>>> getNotes(
+      @RequestParam(defaultValue = "0") int page,
+      @RequestParam Long bookId,
+      @AuthenticationPrincipal UserEntity currentUser
+  ) {
+    PageResult<NoteOutput> outputPageResult = getNotePaginatorUseCase.execute(bookId, currentUser.getId(), page);
+    PageResult<NoteResponse> responsePageResult = new PageResult<>(
+        outputPageResult.pageSize(),
+        outputPageResult.totalPages(),
+        outputPageResult.currentPage(),
+        outputPageResult.data().stream().map(noteMapper::outputToResponse).toList()
+    );
+    return ResponseEntity.ok(ApiResponse.success(responsePageResult));
   }
 }
