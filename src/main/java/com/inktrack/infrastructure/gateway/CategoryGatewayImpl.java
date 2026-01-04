dfd600d8f70e2094482c32efb8ab539716bda12f
Felipe Melo <felipemelozx@gmail.com>
2026-01-04 13:52:15 -0300

    feat: Implement GetAllCategoryUseCase and CategoryGatewayImpl.getAll

--- src/main/java/com/inktrack/infrastructure/gateway/CategoryGatewayImpl.java
+++ src/main/java/com/inktrack/infrastructure/gateway/CategoryGatewayImpl.java
@@ -7,6 +7,7 @@ import com.inktrack.infrastructure.mapper.CategoryMapper;
 import com.inktrack.infrastructure.persistence.CategoryRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -24,5 +25,11 @@ public class CategoryGatewayImpl implements CategoryGateway {
   public Optional<Category> getById(Long id) {
     Optional<CategoryEntity> optionalCategory = categoryRepository.findById(id);
     return optionalCategory.map(categoryMapper::entityToDomain);
+  }
+
+  @Override
+  public List<Category> getAll() {
+    List<CategoryEntity> categoryEntityList = categoryRepository.findAll();
+    return categoryEntityList.stream().map(categoryMapper::entityToDomain).toList();
   }
 }
