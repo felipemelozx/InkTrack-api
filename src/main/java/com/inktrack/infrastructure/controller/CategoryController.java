c8b8684d2b5f66aebaa258c4de44e785e67b9aed
Felipe Melo <felipemelozx@gmail.com>
2026-01-04 13:22:28 -0300

    feat: add CategoryController for '/categories' endpoints

---
+++ src/main/java/com/inktrack/infrastructure/controller/CategoryController.java
@@ -0,0 +1,35 @@
+package com.inktrack.infrastructure.controller;
+
+import com.inktrack.core.usecases.category.CategoryOutput;
+import com.inktrack.core.usecases.category.GetCategoryByIdUseCase;
+import com.inktrack.infrastructure.dtos.category.CategoryResponse;
+import com.inktrack.infrastructure.mapper.CategoryMapper;
+import com.inktrack.infrastructure.utils.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+  private final GetCategoryByIdUseCase getCategoryByIdUseCase;
+  private final CategoryMapper categoryMapper;
+
+  public CategoryController(GetCategoryByIdUseCase getCategoryByIdUseCase, CategoryMapper categoryMapper) {
+    this.getCategoryByIdUseCase = getCategoryByIdUseCase;
+    this.categoryMapper = categoryMapper;
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<ApiResponse<CategoryResponse>> getCategoryById(
+      @PathVariable Long id
+  ) {
+    CategoryOutput categoryOutput = getCategoryByIdUseCase.execute(id);
+    CategoryResponse categoryResponse = categoryMapper.outputToResponse(categoryOutput);
+    ApiResponse<CategoryResponse> apiResponse = ApiResponse.success(categoryResponse);
+    return ResponseEntity.ok(apiResponse);
+  }
+}
