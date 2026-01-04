5606acce47e40aeee64517dd0504668312c12043
Felipe Melo <felipemelozx@gmail.com>
2026-01-04 13:52:54 -0300

    feat: Integrate GetAllCategoryUseCase into BeansConfig and CategoryController

--- src/main/java/com/inktrack/infrastructure/controller/CategoryController.java
+++ src/main/java/com/inktrack/infrastructure/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.inktrack.infrastructure.controller;
 
 import com.inktrack.core.usecases.category.CategoryOutput;
+import com.inktrack.core.usecases.category.GetAllCategoryUseCase;
 import com.inktrack.core.usecases.category.GetCategoryByIdUseCase;
 import com.inktrack.infrastructure.dtos.category.CategoryResponse;
 import com.inktrack.infrastructure.mapper.CategoryMapper;
@@ -11,15 +12,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
 
   private final GetCategoryByIdUseCase getCategoryByIdUseCase;
+  private final GetAllCategoryUseCase getAllCategoryUseCase;
   private final CategoryMapper categoryMapper;
 
-  public CategoryController(GetCategoryByIdUseCase getCategoryByIdUseCase, CategoryMapper categoryMapper) {
+  public CategoryController(
+      GetCategoryByIdUseCase getCategoryByIdUseCase,
+      GetAllCategoryUseCase getAllCategoryUseCase,
+      CategoryMapper categoryMapper
+  ) {
     this.getCategoryByIdUseCase = getCategoryByIdUseCase;
+    this.getAllCategoryUseCase = getAllCategoryUseCase;
     this.categoryMapper = categoryMapper;
   }
 
@@ -30,6 +39,16 @@ public class CategoryController {
     CategoryOutput categoryOutput = getCategoryByIdUseCase.execute(id);
     CategoryResponse categoryResponse = categoryMapper.outputToResponse(categoryOutput);
     ApiResponse<CategoryResponse> apiResponse = ApiResponse.success(categoryResponse);
+    return ResponseEntity.ok(apiResponse);
+  }
+
+  @GetMapping
+  public ResponseEntity<ApiResponse<List<CategoryResponse>>> getAllCategories() {
+    List<CategoryOutput> categoryOutputs = getAllCategoryUseCase.execute();
+    List<CategoryResponse> categoryResponseList = categoryOutputs.stream()
+        .map(categoryMapper::outputToResponse)
+        .toList();
+    ApiResponse<List<CategoryResponse>> apiResponse = ApiResponse.success(categoryResponseList);
     return ResponseEntity.ok(apiResponse);
   }
 }
