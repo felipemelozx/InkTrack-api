195bb43085763c2e1103d7f0e833fcd2d3b9c3fa
Felipe Melo <felipemelozx@gmail.com>
2026-01-04 19:17:24 -0300

    feat: integrate category into book use cases

--- src/main/java/com/inktrack/core/usecases/book/CreateBookUseCaseImpl.java
+++ src/main/java/com/inktrack/core/usecases/book/CreateBookUseCaseImpl.java
@@ -1,16 +1,21 @@
 package com.inktrack.core.usecases.book;
 
 import com.inktrack.core.domain.Book;
+import com.inktrack.core.domain.Category;
 import com.inktrack.core.domain.User;
 import com.inktrack.core.gateway.BookGateway;
+import com.inktrack.core.gateway.CategoryGateway;
+import com.inktrack.core.usecases.category.CategoryOutput;
 import com.inktrack.core.usecases.user.UserOutput;
 
 public class CreateBookUseCaseImpl implements CreateBookUseCase {
 
   private final BookGateway bookGateway;
+  private final CategoryGateway categoryGateway;
 
-  public CreateBookUseCaseImpl(BookGateway bookGateway) {
+  public CreateBookUseCaseImpl(BookGateway bookGateway, CategoryGateway categoryGateway) {
     this.bookGateway = bookGateway;
+    this.categoryGateway = categoryGateway;
   }
 
   @Override
@@ -26,8 +31,13 @@ public class CreateBookUseCaseImpl implements CreateBookUseCase {
     if(modelInput.title().isBlank() || modelInput.author().isBlank()) {
       throw new IllegalArgumentException("The title and author not can be black or null.");
     }
+
+    Category category = categoryGateway.getById(modelInput.categoryId())
+        .orElseThrow(() -> new IllegalArgumentException("Category not found with id: " + modelInput.categoryId()));
+
     Book book = Book.builder()
         .user(currentUser)
+        .category(category)
         .title(modelInput.title())
         .author(modelInput.author())
         .totalPages(modelInput.totalPages())
@@ -40,9 +50,15 @@ public class CreateBookUseCaseImpl implements CreateBookUseCase {
         bookSaved.getUser().getEmail(),
         bookSaved.getUser().getCreatedAt()
     );
+    CategoryOutput categoryOutput = new CategoryOutput(
+        bookSaved.getCategory().id(),
+        bookSaved.getCategory().name(),
+        bookSaved.getCategory().createdAt()
+    );
     return new BookModelOutput(
         bookSaved.getId(),
         userOutput,
+        categoryOutput,
         bookSaved.getTitle(),
         bookSaved.getAuthor(),
         bookSaved.getTotalPages(),
--- src/main/java/com/inktrack/core/usecases/book/UpdateBookUseCaseImpl.java
+++ src/main/java/com/inktrack/core/usecases/book/UpdateBookUseCaseImpl.java
@@ -2,6 +2,8 @@ package com.inktrack.core.usecases.book;
 
 import com.inktrack.core.domain.Book;
 import com.inktrack.core.gateway.BookGateway;
+import com.inktrack.core.gateway.CategoryGateway;
+import com.inktrack.core.usecases.category.CategoryOutput;
 import com.inktrack.core.usecases.user.UserOutput;
 
 import java.time.OffsetDateTime;
@@ -10,18 +12,24 @@ import java.util.UUID;
 public class UpdateBookUseCaseImpl implements UpdateBookUseCase{
 
   private final BookGateway bookGateway;
+  private final CategoryGateway categoryGateway;
 
-  public UpdateBookUseCaseImpl(BookGateway bookGateway) {
+  public UpdateBookUseCaseImpl(BookGateway bookGateway, CategoryGateway categoryGateway) {
     this.bookGateway = bookGateway;
+    this.categoryGateway = categoryGateway;
   }
 
   @Override
   public BookModelOutput execute(Long id, BookModelInput modelInput, UUID userId) {
     Book book = bookGateway.findByIdAndUserId(id, userId);
 
+    var category = categoryGateway.getById(modelInput.categoryId())
+        .orElseThrow(() -> new IllegalArgumentException("Category not found with id: " + modelInput.categoryId()));
+
     Book bookUpdated = Book.builder()
         .id(book.getId())
         .user(book.getUser())
+        .category(category)
         .title(modelInput.title())
         .author(modelInput.author())
         .totalPages(modelInput.totalPages())
@@ -37,9 +45,15 @@ public class UpdateBookUseCaseImpl implements UpdateBookUseCase{
         savedBook.getUser().getEmail(),
         savedBook.getUser().getCreatedAt()
     );
+    CategoryOutput categoryOutput = new CategoryOutput(
+        savedBook.getCategory().id(),
+        savedBook.getCategory().name(),
+        savedBook.getCategory().createdAt()
+    );
     return new BookModelOutput(
         savedBook.getId(),
         userOutput,
+        categoryOutput,
         savedBook.getTitle(),
         savedBook.getAuthor(),
         savedBook.getTotalPages(),
